85c744136eb739e00bd62ecc76e6d55bd5f284ff
Hi30MC <duimstraash@gmail.com>
2026-08-09 23:12:55 -0400

    unique atom textures

--- Atoms.cs
+++ Atoms.cs
@@ -10,84 +10,84 @@ public static class Atoms {
             ID: 201,
             modName: "Origami",
             name: "Identity",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/id_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         three = Brimstone.API.CreateNormalAtom(
             ID: 203,
             modName: "Origami",
             name: "Three",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/three_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         four = Brimstone.API.CreateNormalAtom(
             ID: 204,
             modName: "Origami",
             name: "Four",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/four_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         seven = Brimstone.API.CreateNormalAtom(
             ID: 207,
             modName: "Origami",
             name: "Seven",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/seven_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         eight = Brimstone.API.CreateNormalAtom(
             ID: 208,
             modName: "Origami",
             name: "Eight",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/eight_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         eleven = Brimstone.API.CreateNormalAtom(
             ID: 211,
             modName: "Origami",
             name: "Eleven",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/eleven_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         twelve = Brimstone.API.CreateNormalAtom(
             ID: 212,
             modName: "Origami",
             name: "Twelve",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/twelve_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         fifteen = Brimstone.API.CreateNormalAtom(
             ID: 215,
             modName: "Origami",
             name: "Fifteen",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/fifteen_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         sixteen = Brimstone.API.CreateNormalAtom(
             ID: 216,
             modName: "Origami",
             name: "Sixteen",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/sixteen_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         nineteen = Brimstone.API.CreateNormalAtom(
             ID: 219,
             modName: "Origami",
             name: "Ninteen",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/ninteen_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         twenty = Brimstone.API.CreateNormalAtom(
             ID: 220,
             modName: "Origami",
             name: "Twenty",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/twenty_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
         twentythree = Brimstone.API.CreateNormalAtom(
             ID: 223,
             modName: "Origami",
             name: "Twenty Three",
-            pathToSymbol: "textures/atoms/Hi30MC/Origami/salt_symbol",
+            pathToSymbol: "textures/atoms/Hi30MC/Origami/twentythree_symbol",
             pathToDiffuse: "textures/atoms/Hi30MC/Origami/copper_diffuse"
         );
 
--- obj/Debug/net452/origami.AssemblyInfo.cs
+++ obj/Debug/net452/origami.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("origami")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b84344591f0ac4f638791802f56c2dd099b911")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d86bbbe4d9694ff157dacb8cfd1c64fd639fdcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("origami")]
 [assembly: System.Reflection.AssemblyTitleAttribute("origami")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
