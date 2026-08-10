416462bf3d291f398996bc99bfda5ed19baea663
Hi30MC <duimstraash@gmail.com>
2026-08-10 12:27:38 -0400

    translation is now a calicinator-type bowl

--- Glyphs.cs
+++ Glyphs.cs
@@ -13,9 +13,7 @@ public static class Glyphs {
     public static readonly HexIndex CompositionOutput = new(-1, 1);
     public static readonly class_256 CompositionBase = Brimstone.API.GetTexture("textures/parts/Hi30MC/Origami/Composition/base");
 
-    public static readonly HexIndex TranslationInput = new (-1,0);
-    public static readonly HexIndex TranslationNop = new (0,0);
-    public static readonly HexIndex TranslationOutput = new (1,0);
+    public static readonly HexIndex TranslationBowl = new (0,0);
     public static readonly class_256 TranslationBase = Brimstone.API.GetTexture("textures/parts/Hi30MC/Origami/Translation/base");
 
     public static void LoadParts() {
@@ -36,11 +34,11 @@ public static class Glyphs {
             name: "Glyph of Translation",
             description: "Translates between neuvolics and ordinals",
             cost: 15,
-            glow: Brimstone.API.GetTexture("textures/select/Hi30MC/Origami/triline_glow"),
-            stroke: Brimstone.API.GetTexture("textures/select/Hi30MC/Origami/triline_stroke"),
+            glow: Brimstone.API.GetTexture("textures/select/Hi30MC/Origami/single_glow"),
+            stroke: Brimstone.API.GetTexture("textures/select/Hi30MC/Origami/single_stroke"),
             icon: Brimstone.API.GetTexture("textures/parts/Hi30MC/Origami/Translation/icon"),
             hoveredIcon: Brimstone.API.GetTexture("textures/parts/Hi30MC/Origami/Translation/icon"),
-            usedHexes: new HexIndex[] { TranslationInput, TranslationNop, TranslationOutput },
+            usedHexes: new HexIndex[] {TranslationBowl},
             customPermission: Origami.TranslationPermission
         );
 
@@ -88,29 +86,7 @@ public static class Glyphs {
             Vector2 centre = TranslationBase.method_691();
             renderer.method_523(TranslationBase, new Vector2(-1, -1), centre, 0f);
 
-            renderer.method_530(class_238.field_1989.field_90.field_255.field_293, TranslationInput, 0);
-
-            int IrisFrame = 15;
-            bool AfterIrisOpens = false;
-            Molecule RisingAtom = null;
-            Vector2 RisingOffset = uco.field_1984 + class_187.field_1742.method_492(TranslationOutput).Rotated(uco.field_1985);
-            renderer.method_528(class_238.field_1989.field_90.field_228.field_272, TranslationOutput, Vector2.Zero);
-
-            if (pss.field_2743) {
-                IrisFrame = class_162.method_404((int) (class_162.method_411(1f, -1f, time) * 16f), 0, 15);
-                AfterIrisOpens = time > 0.5f;
-                RisingAtom = Molecule.method_1121(pss.field_2744[0]);
-                if (!AfterIrisOpens) {
-                    Editor.method_925(RisingAtom, RisingOffset, new HexIndex (0, 0), 0f, 1f, time, 1f, false, null);
-                }
-            }
-
-            renderer.method_529(class_238.field_1989.field_90.field_246[IrisFrame], TranslationOutput, Vector2.Zero);
-            renderer.method_528(class_238.field_1989.field_90.field_228.field_271, TranslationOutput, Vector2.Zero);
-
-            if (pss.field_2743 && AfterIrisOpens) {
-                Editor.method_925(RisingAtom, RisingOffset, new HexIndex (0, 0), 0f, 1f, time, 1f, false, null);
-            }
+            renderer.method_528(class_238.field_1989.field_90.field_170, TranslationBowl, Vector2.Zero);
         });
 
         QApi.RunDuringCycle(static (sim, part, pss, first) => {
@@ -160,13 +136,8 @@ public static class Glyphs {
             {
                 if (first)
                 {
-                    HexIndex hole = (part.method_1184(TranslationInput));
-                    HexIndex iris = (part.method_1184(TranslationOutput));
-                    if (sim.FindAtom(iris).method_1085())
-                    {
-                        return;
-                    }
-                    if (!sim.FindAtom(hole).method_99(out AtomReference subject) || subject.field_2281 || subject.field_2282)
+                    HexIndex bowl = (part.method_1184(TranslationBowl));
+                    if (!sim.FindAtom(bowl).method_99(out AtomReference subject))
                     {
                         return;
                     }
@@ -175,15 +146,8 @@ public static class Glyphs {
                         return;
                     }
 
-                    Brimstone.API.RemoveAtom(subject);
-                    Brimstone.API.DrawFallingAtom(seb, subject);
-
-                    pss.field_2743 = true;
-                    pss.field_2744 = new AtomType[] { output };
-                }
-                else if (pss.field_2743)
-                {
-                    Brimstone.API.AddAtom(sim, part, TranslationOutput, pss.field_2744[0]);
+                    Brimstone.API.ChangeAtom(subject, output);
+                    subject.field_2279.field_2276 = new class_168(seb, 0, (enum_132) 1, subject.field_2280, class_238.field_1989.field_81.field_614, 30f);
                 }
             }
         });
--- obj/Debug/net452/origami.AssemblyInfo.cs
+++ obj/Debug/net452/origami.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("origami")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c744136eb739e00bd62ecc76e6d55bd5f284ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4573773f72bb5fd535ce886251705eeac91aec")]
 [assembly: System.Reflection.AssemblyProductAttribute("origami")]
 [assembly: System.Reflection.AssemblyTitleAttribute("origami")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
