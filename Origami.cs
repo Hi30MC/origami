db798348a82f7787697faccb8554627d321d6924
Hi30MC <duimstraash@gmail.com>
2026-08-23 13:53:57 -0400

    mirror and map rules

--- Origami.cs
+++ Origami.cs
@@ -12,18 +12,23 @@ public class Origami: QuintessentialMod {
 
     public static bool NeuvolicsLoaded = Brimstone.API.IsModLoaded("Neuvolics");
     public static bool SennmetalsLoaded = Brimstone.API.IsModLoaded("Sennmetals");
+    public static bool FTSIGCTULoaded = Brimstone.API.IsModLoaded("FTSIGCTU");
 
     public override void Load () {
         Logger.Log("Origami: Paper ready to fold!");
     }
 
-    public override void PostLoad() {}
+    public override void PostLoad()
+    {
+        LoadMirrorRules();
+    }
 
     public override void Unload() {
         Glyphs.RemoveHooks();
     }
 
-    public override void LoadPuzzleContent() {
+    public override void LoadPuzzleContent()
+    {
         Atoms.LoadAtoms();
         Wheel.LoadWheel();
         Glyphs.AddHooks();
@@ -37,10 +42,31 @@ public class Origami: QuintessentialMod {
         QApi.AddPuzzlePermission(SacrificePermission, "Glyph of Sacrifice", "Origami");
 
         if (NeuvolicsLoaded) ImportManager.ImportNeuvolics();
-        if (SennmetalsLoaded) {ImportManager.ImportSennmetals(); Logger.Log("loaded sennmetals");}
+        if (SennmetalsLoaded) { ImportManager.ImportSennmetals(); Logger.Log("loaded sennmetals"); }
         if (NeuvolicsLoaded || SennmetalsLoaded) QApi.AddPuzzlePermission(TranslationPermission, "Glyph of Translation", "Origami");
+
+        if (FTSIGCTULoaded)
+        {
+            LoadMapRules();
+        }
 
         GlyphLUT.GenerateLUTs();
         Logger.Log("Origami: Assets loaded :3c");
+    }
+
+    private static void LoadMapRules()
+    {
+        FTSIGCTU.Navigation.PartsMap.addPartHexRule(Glyphs.Composition, FTSIGCTU.Navigation.PartsMap.glyphRule);
+        FTSIGCTU.Navigation.PartsMap.addPartHexRule(Glyphs.AugmentedComp, FTSIGCTU.Navigation.PartsMap.glyphRule);
+        FTSIGCTU.Navigation.PartsMap.addPartHexRule(Glyphs.HighRegeneration, FTSIGCTU.Navigation.PartsMap.glyphRule);
+        FTSIGCTU.Navigation.PartsMap.addPartHexRule(Glyphs.LowRegeneration, FTSIGCTU.Navigation.PartsMap.glyphRule);
+        FTSIGCTU.Navigation.PartsMap.addPartHexRule(Glyphs.Translation, FTSIGCTU.Navigation.PartsMap.glyphRule);
+        // the glyph that does not exist does not appear on the map.
+    }
+
+    private static void LoadMirrorRules() {
+        FTSIGCTU.MirrorTool.addRule(Glyphs.LowRegeneration, FTSIGCTU.MirrorTool.mirrorSimplePart);
+        FTSIGCTU.MirrorTool.addRule(Glyphs.Translation, FTSIGCTU.MirrorTool.mirrorSimplePart);
+        FTSIGCTU.MirrorTool.addRule(Glyphs.Sacrifice, FTSIGCTU.MirrorTool.mirrorSimplePart);
     }
 }
